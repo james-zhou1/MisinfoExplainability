5fbfba67fcbc9766dd60fa234a9cce24971fe948
James Zhou <jaems.zhou@berkeley.edu>
2023-10-17 12:36:56 -0700

    Script to create dataset around text, classification, and explanation

--- prompts.py
+++ prompts.py
@@ -1,19 +1,25 @@
 import openai
+import pandas as pd
 
 with open('key.env', 'r') as file:
     API_KEY = file.read().strip().split('=')[1]
 
 openai.api_key = API_KEY
 
-import pandas as pd
 
+# Load prompts to analyze
 data = pd.read_csv('CT-FAN-22\English_data_test_release_with_rating.csv')
-prompts = data['text'].tolist()[:5]
+prompts = data['text'].tolist()[:1]
 
-# print(prompts[0])
-
-tones = ["neutral", "emotional", "authoritative", "casual"]
+# Define the types of explanations you would like to generate.
+tones = ["neutral", "emotional"]#, "authoritative", "casual"]
 scores = {}
+
+response = openai.Completion.create(
+      engine="text-davinci-003",
+      prompt=f"Classify the following statement as true, mostly-true, half-true, mostly-false, false, or pants-fire. Then, explain your classification.",
+      max_tokens=60
+    ).choices[0].text.strip()
 
 for prompt in prompts:
   for tone in tones:
@@ -25,13 +31,12 @@ for prompt in prompts:
 
     score = openai.Completion.create(
       engine="text-davinci-003",
-      prompt=f"Provide a score for this statement from 0 to 100, where 0 represents a poor explanation and 100 represents a good explanation. Do not provide any explanations, only respond with the numerical score: {response}",
+      prompt=f"Provide a score for this statement from 0 to 100, where 0 represents a poor explanation and 100 represents a good explanation.",# Do not provide any explanations, only respond with the numerical score: {response}",
       max_tokens=60 
     ).choices[0].text.strip()
 
-    print(tone, score)
-    print(response)
-    print("\n")
+    print("Tone", tone)
+    print("Score", score)
 
     scores.setdefault(tone, []).append(score)
 
